56e686c263f4aa39b0764afcffd99cf14e4c00be
Maxime Decourtray <mdecourtray11@gmail.com>
2025-01-12 10:34:32 +0100

    fix: add dist to gitignore

--- dist-client/assets/client-home-B-pW5Vy9.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./Home-BLQNxmZQ.js";window.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("home_component");e&&new o({target:e,hydrate:!0,props:{dataForHydration:window.home_component}})});
